6188024067e6aad68dda7d963def1021577d7425
William Oliveira <sudowilliam@gmail.com>
2020-04-04 03:04:27 -0300

    Simple ts file to begin with

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('hey ts')
+const button = document.querySelector('input[type=submit]')
+const input1 = document.getElementById('number1')! as HTMLInputElement
+const input2 = document.getElementById('number2')! as HTMLInputElement
+
+function add(num1: number, num2: number) {
+  return num1 + num2
+}
+
+button.addEventListener('click', function () {
+  console.log('clicked')
+  console.log(add(+input1.value, +input2.value))
+})
